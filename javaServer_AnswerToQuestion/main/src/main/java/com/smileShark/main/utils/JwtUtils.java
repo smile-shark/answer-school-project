e80b6e85abe6d49b0bd60e8d3df69bb8b4e8438a
smile-shark <488089248@qq.com>
2025-03-07 10:16:53 +0800

    添加查看操作用户的日志

--- javaServer_AnswerToQuestion/main/src/main/java/com/smileShark/main/utils/JwtUtils.java
+++ javaServer_AnswerToQuestion/main/src/main/java/com/smileShark/main/utils/JwtUtils.java
@@ -13,13 +13,14 @@ import java.util.Map;
 public class JwtUtils {
     public static User parseJWT(String jwt) {
         Claims claims= Jwts.parser()
-                .setSigningKey("www.smileShark.com")
+                .setSigningKey("www.tool.shark.com")
                 .parseClaimsJws(jwt)
                 .getBody();
         User user=new User();
         user.setUserId(claims.get("userId").toString());
         user.setUserPassword(claims.get("password").toString());
         user.setUserName(claims.get("userName").toString());
+        System.out.println("操作用户用户："+user.getUserName());
         return user;
     }
     public static String createJwt(String userId,String password,String userName){
@@ -28,7 +29,7 @@ public class JwtUtils {
         claims.put("password",password);
         claims.put("userName",userName);
         return Jwts.builder()
-                .signWith(SignatureAlgorithm.HS256,"www.smileShark.com")
+                .signWith(SignatureAlgorithm.HS256,"www.tool.shark.com")
                 .setClaims(claims)
                 .setExpiration(new Date(System.currentTimeMillis()+1000*60*60*12))
                 .compact();
